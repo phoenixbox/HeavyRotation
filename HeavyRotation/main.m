2c8b6cd508ea5821beefaf55e8ec7a2bd02bcf93
phoenixbox <shane@quickleft.com>
2013-11-20 13:18:19 -0700

    Initial Commit

---
+++ HeavyRotation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HeavyRotation
+//
+//  Created by Shane Rogers on 11/20/13.
+//  Copyright (c) 2013 Shane Rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RotationAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RotationAppDelegate class]));
+    }
+}
